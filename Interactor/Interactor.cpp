92df277156724370f50f81919a87de523bc27fa6
valnig <valentin.nigolian@epfl.ch>
2019-03-07 13:35:00 +0900

    working on eventDispatcher

--- Interactor/Interactor.cpp
+++ Interactor/Interactor.cpp
@@ -1,12 +1,11 @@
+
+#include "pch.h"
+#include <vector>
 #include <iostream>
 #include <utility>
 #include <vector>
 #include <string>
 #include <stdexcept>
-
-
-//Boy Is It Ever Fun To Publish Stuff On Your Own -> BIIEFTPSOYO
-
 
 
 /** prototype stuff*/
@@ -31,6 +30,29 @@ public:
 		std::cout << "updated vertex " << vertex<< " as "<<label << std::endl;
 		return true;
 	}
+
+	bool remove(Vertex vertex) {
+		//TODO : remove vertex
+		std::cout << "removed vertex " << vertex << std::endl;
+		return true;
+	}
+
+	bool remove(Edge edge) {
+		std::cout << "removed edge " << to_string(edge) << std::endl;
+		return true;
+	}
+
+	std::string to_string(Vertex vertex) {
+		return "vertex "+std::to_string(vertex);
+	}
+
+	std::string to_string(Edge edge) {
+		return "edge ("+std::to_string(edge.first) + "->"+ std::to_string(edge.second)+")";
+	}
+
+	~Graph() {
+		std::cout << " deleted graph " << this << std::endl;
+	}
 };
 
 
@@ -45,39 +67,39 @@ protected:
 	_STATUS status_;///< default status ? Good really ?
 };
 
-
-class MoveVertex : public Tool<Graph, std::string>{
-	Vertex vertex_;
+template<typename _WHAT>
+class Remove : public Tool<Graph, std::string>{
+	_WHAT graph_element_;
 
 public:
 	
-	void start(Graph* graph, Vertex vertex) {
+	void start(Graph* graph, _WHAT graph_element) {
 		subject_ = graph;
-		vertex_ = vertex;
-		std::cout << "vertex " << vertex << " of graph " << graph << " will be updated " << std::endl;
-		status_ = true;
+		graph_element_ = graph_element;
+		std::cout << graph->to_string(graph_element) << " of graph " << graph << " will be removed " << std::endl;
+		status_ = "started";
 	}
 
-	void tick(VertexLabel label) {
-		subject_->update_vertex(vertex_, label);
-		status_ = true;
+	void tick() {
+		status_ = "ticking";
 	}
 
 	void stop() {
 		//TODO: do something interesting
-		status_ = true;
+		subject_->remove(graph_element_);
+		status_ = "stopped";
 	}
 };
 
 
 template<typename _SUBJECT, typename _TOOL>
-class Operation {
+class Interaction {
 	_TOOL* tool_;
 	
 public:
 
 	template<typename ... _ARGS>
-	Operation(_ARGS ... args) {
+	Interaction(_ARGS ... args) {
 		tool_ = new _TOOL();
 		tool_->start(args...);
 	}
@@ -85,12 +107,17 @@ public:
 
 	template<typename ... _ARGS>
 	void tick(_ARGS ... args) {
-		tool_->tick(args...);
+		if (tool_) {
+			tool_->tick(args...);
+		}
 	}
 
 	template<typename ... _ARGS>
 	void stop(_ARGS ... args) {
-		tool_->stop(args...);
+		if (tool_) {
+			tool_->stop(args...);
+			delete tool_;
+		}
 	}
 
 };
@@ -99,18 +126,22 @@ template<typename _SUBJECT>
 class Interactor {
 private:
 	_SUBJECT* subject_;
+	const bool allocated_subject_;
 	
 public:
 
-	Interactor(_SUBJECT* subject) : subject_(subject) {}
+	Interactor(_SUBJECT* subject) : subject_(subject), allocated_subject_(false) {}
+
+	template<typename ... _ARGS>
+	Interactor(_ARGS ... args) : subject_(new _SUBJECT(args...)), allocated_subject_(true) {}
 
 	template<typename _TOOL, typename ... _ARGS>
-	Operation<_SUBJECT, _TOOL>* start_operation(_ARGS ... args) {
-		return new Operation<_SUBJECT, _TOOL>(subject_, args...);
+	Interaction<_SUBJECT, _TOOL>* start_operation(_ARGS ... args) const {
+		return new Interaction<_SUBJECT, _TOOL>(subject_, args...);
 	}
 
 	template<typename _TOOL, typename ... _ARGS>
-	void tick_operation(Operation<_SUBJECT, _TOOL>* op, _ARGS ... args) {
+	void tick_operation(Interaction<_SUBJECT, _TOOL>* op, _ARGS ... args) const {
 		if(!op){
 			throw std::invalid_argument("ticked nullptr Operation");
 		}
@@ -118,71 +149,120 @@ public:
 	}
 	
 	template<typename _TOOL, typename ... _ARGS>
-	void stop_operation(Operation<_SUBJECT, _TOOL>* op, _ARGS ... args) {
+	void stop_operation(Interaction<_SUBJECT, _TOOL>* op, _ARGS ... args) const {
+		if (!op) {
+			throw std::invalid_argument("stopped nullptr Operation");
+		}
 		op->stop(args...);
+		delete op;
+	}
+
+	~Interactor() {
+		if (allocated_subject_) {
+			delete subject_;
+		}
 	}
 };
 
-/** This is supposed to be a dispatcher for user input events.
- * Basically it can contain multiple Interactors to perform any kind of 
- * operations on any kind of objects.
- * This class is an example
- * TODO : Is that the worst name you ever heard for a class or what ?*/
-class UserEventDispatcher{
+#if 0
+template<typename ... _SUBJECTS>
+class Context {
+
+};
+
+template<typename ... _TOOLS>
+class Toolset {
+
+};
+
+template<typename _CONTEXT, typename _TOOLSET, enum INTERACTION_MODE>
+class SingleInteractionMouseEventDispatcher {
+private:
+	_CONTEXT* context_;
+	void* current_interaction_ = nullptr;
+
+public:
+
+	SingleInteractionMouseEventDispatcher(_CONTEXT* context) : context_(context) {}
+
+	template<typename _TOOL>
+	void set_mode() {
+
+	}
+};
+#endif
+
+
+/** This is supposed to be a dispatcher for mouse input events.
+* Basically it can contain multiple Interactors to perform any kind of
+* operations on any kind of objects.
+This class is an example of how to use Interactors*/
+class MouseEventDispatcher{
 	public:
 	
-	typedef enum{MODE_ONE, MODE_TWO, MODE_THREE} INTERACTION_MODE;
+		typedef enum {MOVE, CUT, REMOVE} INTERACTION_MODE;
 	
 	private:
-	Graph graph_;
+	Graph* graph_;
 	INTERACTION_MODE mode_;
-	Interactor<Graph> interactor_;
+	const Interactor<Graph> interactor_;
+	std::vector<void*> interactions_;
 	
 
 	public:
 	
-	UserInputToCodeInterface() : {}
+	MouseEventDispatcher(Graph* graph) : graph_(graph), interactor_(graph) {} //, interactor2_({}, { {},{} }) {}
 	
+	bool set_mode(INTERACTION_MODE mode) {
+		mode_ = mode;
+		return true;
+	}
+
 	/** _WHAT defines on what the user clicked*/
 	template<typename _WHAT>
 	void mouse_click(_WHAT what){
-		Vertex v = what.vertex();
 		switch (mode_){
-			case MODE_ONE : interactor_.start_operation<MoveVertex>(v); break;
+			case REMOVE : interactions_.push_back((Interaction<Graph,Remove<_WHAT>>*)interactor_.start_operation<Remove<_WHAT>>(what)); break;
 			default : {//do nothing
 			break;}
 		}
 	}
 	
-	void tick();
+	void tick() {}
+
 };
+
 
 int main()
 {
+	std::vector<VertexLabel> verts({ 1,2,3 });
+	std::vector<Edge> edges({ {1,2},{2,3} });
+
+	Graph graph(verts, edges);
+
+	MouseEventDispatcher dispatcher(&graph);
+	dispatcher.set_mode(MouseEventDispatcher::REMOVE);
+
+	dispatcher.mouse_click<Vertex>(1);
+	dispatcher.mouse_click<Edge>(Edge({ 1,2 }));
 	
-	
-	Graph graph({ 1,2,3 }, { {1,2},{2,3} });
+#if 0
 
 	Interactor<Graph> interactor(&graph);
-	Operation<Graph, MoveVertex>* op = interactor.start_operation<MoveVertex>(1);
+
+	Interactor<Graph> interactor2(verts, edges);
+
+	Interaction<Graph, Remove<Vertex>>* interaction = interactor.start_operation<Remove<Vertex>>(1);
+	interaction->tick();
+	interaction->stop();
+
+	Interaction<Graph, Remove<Edge>>* interaction2 = interactor.start_operation<Remove<Edge>>(Edge({ 1,2 }));
 	
-	interactor.tick_operation(op,10);
-	
-	interactor.stop_operation(op);
+	interaction2->tick();
+	interaction2->stop();
+#endif
 
 	return 0;
 
-	/*Operation<Graph, MoveVertex> op;
-	bool result = op.start<bool,Graph*,Vertex>(&graph, 1);
-
-
-	
-	for (int i(0); i < 3; i++) {
-		op.tick<bool, VertexLabel>(i);
-	}
-
-	op.stop<bool>();
-	
-	return 0;*/
 }
 
